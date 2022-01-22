7f9dc5839c3609f6087d92b65343c93a45050685
Christian Jans <cjjans@ualberta.ca>
2022-01-22 14:34:15 -0700

    Clean up main

--- Main.java
+++ Main.java
@@ -2,24 +2,20 @@ import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
-        // System.out.println(ShareUtil.getPrice("AAPL"));
-
         if (args.length > 0 && args[0].equals("--parallel")) {
             // Run the parallel timing tests.
+            System.out.println("Running functional in parallel...");
             parallelTimingTests();
         } else {
             // Run the sequential timing tests.
+            System.out.println("Running functional...");
             sequentialTimingTests();
         }
     }
 
     private static void parallelTimingTests() {
         long start = System.nanoTime();
-
-        System.out.println("Parallel");
-        // PickShareFunctional.findHighPriced(Shares.symbols.parallelStream());
-
+        PickShareFunctional.findHighPriced(Shares.symbols.parallelStream());
         long end = System.nanoTime();
 
         System.out.println(
@@ -32,10 +28,7 @@ public class Main {
 
     private static void sequentialTimingTests() {
         long start = System.nanoTime();
-
-        System.out.println("Sequential");
-        // PickShareFunctional.findHighPriced(Shares.symbols.stream());
-
+        PickShareFunctional.findHighPriced(Shares.symbols.stream());
         long end = System.nanoTime();
 
         System.out.println(
